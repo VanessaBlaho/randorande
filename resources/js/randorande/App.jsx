d0ae91efc39bf037fb3ca489274b167728f75a54
Vanessa Blaho <vanessadblaho@gmail.com>
2023-11-13 14:18:25 +0100

    basic footer set up

--- resources/js/randorande/App.jsx
+++ resources/js/randorande/App.jsx
@@ -1,4 +1,7 @@
+import Footer from "./components/Footer";
+
 export default function App() {
     return <>
-    <h1>RandoRande</h1></>
+    <h1>RandoRande</h1>
+    <Footer/></>
 }
---
+++ resources/js/randorande/components/Footer.jsx
@@ -0,0 +1,31 @@
+import '../../../scss/_footer.scss'
+
+
+export default function Footer (){ 
+return (
+    <div class="footer">
+    <div class="footer-col">
+      <ul>
+        <li><a href="#">Home</a></li>
+        <li><a href="#">Dates </a></li>
+        <li><a href="#">Profile</a></li>
+        <li><a href="#">About Us</a></li>
+        <li><a href="#">FAQ</a></li>
+      </ul>
+    </div>
+  
+    <div class="footer-col-contactUs">
+      <ul>
+        <li><a href="#">Contact Us</a></li>
+        <li>Follow Us</li>
+        <li><a href="#">#</a></li>
+        <li><a href="#">#</a></li>
+        <li><a href="#">#</a></li>
+      </ul>
+    </div>
+  
+    <div class="copyright">&copy; 2023</div>
+  </div>
+  
+)
+}
