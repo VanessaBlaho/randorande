2d85fb9d4f5e7bf745e7c129dcdd178fa752118d
Vanessa Blaho <vanessadblaho@gmail.com>
2023-11-14 13:21:05 +0100

    responsive design complete, just need to fix desktop

--- resources/js/randorande/components/RevealedDateDetail.jsx
+++ resources/js/randorande/components/RevealedDateDetail.jsx
@@ -1,12 +1,13 @@
 
-import React, { useEffect, useState } from react
+import React, { useEffect, useState } from 'react'
 
 export function RevealedDateDetail () {
     const [rande,setRande] = useState(null)
 
         const fetchRandeDetail = async () => {
             try {
-                const response = await fetch('http://www.randorande.test/api/randes/{rande_id}');
+                let rande_id = 1
+                const response = await fetch(`/api/randes/${rande_id}`);
                 const data = await response.json();
                 console.log('Data:', data);
                 setRande(data);
@@ -20,14 +21,32 @@ export function RevealedDateDetail () {
         }, [])
 
     return(
-        <></>
-    //     <div className="date-detail">
-    //         <h1>Date name:  {rande.name}</h1>
-    //         <img src={rande.image_path} alt={rande.name} />
-    //         <br/>
-    //         <p> {rande.description}
-    //             </p>
-    // </div>
+        
+        <div className="date-detail"> 
+        {
+            rande
+            ?(
+                <>
+        
+                <h1>Date name:  {rande.name}</h1>
+                <img src={rande.image_path} alt={rande.name} />
+            <br/>
+            <p> {rande.description}
+                </p>
+                </>
+            )
+            :'Loading...'
+        }
+        </div>
+       
     )
 }
 
+ {/* <h1>Date name:  {rande.name}</h1>
+            <img src={rande.image_path} alt={rande.name} />
+            <br/>
+            <p> {rande.description}
+                </p> */}
+
+
+            
