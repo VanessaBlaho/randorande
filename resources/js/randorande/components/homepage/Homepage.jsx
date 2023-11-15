7264bb845e813acaf0330e3c0da5aea9dba4ba79
mariecotije <kkmarushka@gmail.com>
2023-11-15 23:22:50 +0100

    fixed homepage content, adjusted scss file

--- resources/js/randorande/components/homepage/Homepage.jsx
+++ resources/js/randorande/components/homepage/Homepage.jsx
@@ -2,14 +2,13 @@ export default function Homepage() {
     return (
         <>
             <div className="homepage">
-                <div className="homepage__logo">
-                    <img src="./logos/Rando-long.svg" alt="randorande logo" />
-                </div>
+                <div className="homepage__logo">LOGO </div>
 
                 <div className="homepage__instructions">
                     <h1 className="homepage__instructions-title">
                         How it works
                     </h1>
+
                     <ul className="homepage__instructions-list">
                         <li className="homepage__instructions-item">
                             What kind of date are you in the mood for?
@@ -30,15 +29,13 @@ export default function Homepage() {
                             Go on your date with your significant other
                         </li>
                         <li className="homepage__instructions-item">
-                            Keep track of your dating journey in your Date Diary
+                            Keep track of your dating journey in your Journal
                         </li>
                         <li className="homepage__instructions-item">
                             Make everlasting memories
                         </li>
                     </ul>
-                </div>
-                <div className="homepage__btn">
-                    <button className="homepage__btn-trigger">
+                    <button className="homepage__instructions-btn-trigger">
                         Gimme ideas!
                     </button>
                 </div>
