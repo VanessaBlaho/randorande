087c1cbda0de3fedc723d744389a5f38ba60a8a9
Ephram Daniels <linguistnick7@gmail.com>
2023-11-16 18:44:19 +0100

    photos

--- resources/js/randorande/components/about-us/AboutUs.jsx
+++ resources/js/randorande/components/about-us/AboutUs.jsx
@@ -2,12 +2,47 @@ import { Link } from "react-router-dom";
 
 export default function AboutUs() {
 
-return ( 
-<>
-        <h1>About Us</h1>;
-        
-
-</>
+return (
+    <>
+        <h1>The RandoRande Team</h1>;
+        <div className="team__container">
+            <div className="team-member__container crisafi">
+                <img
+                    className="team-member_img"
+                    src="/images/about-us/1.png"
+                    alt="Lauren Crisafi"
+                />
+                <p className="team-member_name">Lauren Crisafi</p>
+            </div>
+            <br />
+            <div className="team-member__container kokoreva">
+                <img
+                    className="team-member_img"
+                    src="/images/about-us/3.png"
+                    alt="Maria Kokoreva"
+                />
+                <p className="team-member_name">Maria Kokoreva</p>
+            </div>
+            <br />
+            <div className="team-member__container blaho">
+                <img
+                    className="team-member_img"
+                    src="/images/about-us/3.png"
+                    alt="Vanessa 'Eats Prague' Blaho"
+                />
+                <p className="team-member_name">Vanessa Blaho</p>
+            </div>
+            <br />
+            <div className="team-member__container daniels">
+                <img
+                    className="team-member_img"
+                    src="/images/about-us/2.png"
+                    alt="Nicholas Ephram Ryan Daniels"
+                />
+                <p className="team-member_name">Ephram Daniels</p>
+            </div>
+        </div>
+    </>
 );
 
 }
