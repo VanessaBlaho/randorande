cdc579f56d87230eb407472f03598f7b168e84c1
Ephram Daniels <linguistnick7@gmail.com>
2023-11-17 12:15:39 +0100

    fixed register styling

--- resources/js/randorande/components/auth/Registration.jsx
+++ resources/js/randorande/components/auth/Registration.jsx
@@ -59,8 +59,12 @@ export default function Register(props) {
     return (
         <>
             <h1 className="register_headline">Create Account</h1>
-            <p className="register_instructions">Already have an account?</p>
-            <button className="register_btn">Login</button>
+            <div className="login_reminder">
+                <p className="register_instructions">
+                    Already have an account?
+                </p>
+                <button className="register_btn">Login</button>
+            </div>
             <br />
             <form
                 className="register_form"
@@ -68,176 +72,207 @@ export default function Register(props) {
                 method="post"
                 onSubmit={handleSubmit}
             >
-                <div className="input_row">
-                    <label htmlFor="first_name">First name</label>
-                    <input
-                        type="text"
-                        name="first_name"
-                        value={values.first_name}
-                        onChange={handleChange}
-                    />
-                </div>
-                <br />
-                {errors.first_name ? (
-                    <>
-                    <div className="errors">
-                        {errors.first_name.map((error, i) => (
-                            <div key={i} className="error">
-                                {error}
-                            </div>
-                        ))}
+                <div className="labels_and_inputs__container">
+                    <div className="input_row">
+                        <label
+                            className="input_row__label"
+                            htmlFor="first_name"
+                        >
+                            First name
+                        </label>
+                        <input
+                            className="input_row__input"
+                            type="text"
+                            name="first_name"
+                            value={values.first_name}
+                            onChange={handleChange}
+                        />
                     </div>
                     <br />
-                    </>
-                ) : (
-                    ""
-                )}
-                <div className="input_row">
-                    <label htmlFor="last_name">Last name</label>
-                    <input
-                        type="text"
-                        name="last_name"
-                        value={values.last_name}
-                        onChange={handleChange}
-                    />
-                </div>
-                <br />
-                {errors.last_name ? (
-                    <>
-                    <div className="errors">
-                        {errors.last_name.map((error, i) => (
-                            <div key={i} className="error">
-                                {error}
+                    {errors.first_name ? (
+                        <>
+                            <div className="errors">
+                                {errors.first_name.map((error, i) => (
+                                    <div key={i} className="error">
+                                        {error}
+                                    </div>
+                                ))}
                             </div>
-                        ))}
+                            <br />
+                        </>
+                    ) : (
+                        ""
+                    )}
+                    <div className="input_row">
+                        <label className="input_row__label" htmlFor="last_name">
+                            Last name
+                        </label>
+                        <input
+                            className="input_row__input"
+                            type="text"
+                            name="last_name"
+                            value={values.last_name}
+                            onChange={handleChange}
+                        />
                     </div>
                     <br />
-                    </>
-                ) : (
-                    ""
-                )}
-                <div className="input_row">
-                    <label htmlFor="username">Username</label>
-                    <input
-                        type="text"
-                        name="username"
-                        value={values.username}
-                        onChange={handleChange}
-                    />
-                </div>
-                <br />
-                {errors.username ? (
-                    <>
-                    <div className="errors">
-                        {errors.username_name.map((error, i) => (
-                            <div key={i} className="error">
-                                {error}
+                    {errors.last_name ? (
+                        <>
+                            <div className="errors">
+                                {errors.last_name.map((error, i) => (
+                                    <div key={i} className="error">
+                                        {error}
+                                    </div>
+                                ))}
                             </div>
-                        ))}
+                            <br />
+                        </>
+                    ) : (
+                        ""
+                    )}
+                    <div className="input_row">
+                        <label className="input_row__label" htmlFor="username">
+                            Username
+                        </label>
+                        <input
+                            className="input_row__input"
+                            type="text"
+                            name="username"
+                            value={values.username}
+                            onChange={handleChange}
+                        />
                     </div>
                     <br />
-                    </>
-                ) : (
-                    ""
-                )}
-                <div className="input_row">
-                    <label htmlFor="email">Email</label>
-                    <input
-                        type="email"
-                        name="email"
-                        value={values.email}
-                        onChange={handleChange}
-                    />
-                </div>
-                <br />
-                {errors.email ? (
-                    <>
-                    <div className="errors">
-                        {errors.email.map((error, i) => (
-                            <div key={i} className="error">
-                                {error}
+                    {errors.username ? (
+                        <>
+                            <div className="errors">
+                                {errors.username_name.map((error, i) => (
+                                    <div key={i} className="error">
+                                        {error}
+                                    </div>
+                                ))}
                             </div>
-                        ))}
+                            <br />
+                        </>
+                    ) : (
+                        ""
+                    )}
+                    <div className="input_row">
+                        <label className="input_row__label" htmlFor="email">
+                            Email
+                        </label>
+                        <input
+                            className="input_row__input"
+                            type="email"
+                            name="email"
+                            value={values.email}
+                            onChange={handleChange}
+                        />
                     </div>
                     <br />
-                    </>
-                ) : (
-                    ""
-                )}
-                <div className="input_row">
-                    <label htmlFor="password">Create password</label>
-                    <input
-                        type="password"
-                        name="password"
-                        value={values.password}
-                        onChange={handleChange}
-                    />
-                </div>
-                <br />
-                {errors.password ? (
-                    <>
-                    <div className="errors">
-                        {errors.password.map((error, i) => (
-                            <div key={i} className="error">
-                                {error}
+                    {errors.email ? (
+                        <>
+                            <div className="errors">
+                                {errors.email.map((error, i) => (
+                                    <div key={i} className="error">
+                                        {error}
+                                    </div>
+                                ))}
                             </div>
-                        ))}
+                            <br />
+                        </>
+                    ) : (
+                        ""
+                    )}
+                    <div className="input_row">
+                        <label className="input_row__label" htmlFor="password">
+                            Create password
+                        </label>
+                        <input
+                            className="input_row__input"
+                            type="password"
+                            name="password"
+                            value={values.password}
+                            onChange={handleChange}
+                        />
                     </div>
                     <br />
-                    </>
-                ) : (
-                    ""
-                )}
-                <div className="input_row">
-                    <label htmlFor="password_confirmation">
-                        Confirm password
-                    </label>
-                    <input
-                        type="password"
-                        name="password_confirmation"
-                        value={values.password_confirmation}
-                        onChange={handleChange}
-                    />
-                </div>
-                <br />
-                {errors.password_confirmation ? (
-                    <>
-                    <div className="errors">
-                        {errors.password_confirmation.map((error, i) => (
-                            <div key={i} className="error">
-                                {error}
+                    {errors.password ? (
+                        <>
+                            <div className="errors">
+                                {errors.password.map((error, i) => (
+                                    <div key={i} className="error">
+                                        {error}
+                                    </div>
+                                ))}
                             </div>
-                        ))}
+                            <br />
+                        </>
+                    ) : (
+                        ""
+                    )}
+                    <div className="input_row">
+                        <label
+                            className="input_row__label"
+                            htmlFor="password_confirmation"
+                        >
+                            Confirm password
+                        </label>
+                        <input
+                            className="input_row__input"
+                            type="password"
+                            name="password_confirmation"
+                            value={values.password_confirmation}
+                            onChange={handleChange}
+                        />
                     </div>
                     <br />
-                    </>
-                ) : (
-                    ""
-                )}
-                <div className="input_row">
-                    <label htmlFor="age_confirmation">I am 18</label>
-                    <input
-                        type="checkbox"
-                        name="age_confirmation"
-                        
-                        onChange={handleChange}
-                    />
-                </div>
-                <br />
-                {errors.age_confirmation ? (
-                    <>
-                    <div className="errors">
-                        {errors.age_confirmation.map((error, i) => (
-                            <div key={i} className="error">
-                                {error}
+                    {errors.password_confirmation ? (
+                        <>
+                            <div className="errors">
+                                {errors.password_confirmation.map(
+                                    (error, i) => (
+                                        <div key={i} className="error">
+                                            {error}
+                                        </div>
+                                    )
+                                )}
                             </div>
-                        ))}
+                            <br />
+                        </>
+                    ) : (
+                        ""
+                    )}
+                    <div className="input_row">
+                        <label
+                            className="input_row__label"
+                            htmlFor="age_confirmation"
+                        >
+                            I'm over 18
+                        </label>
+                        <input
+                            className="input_row__input"
+                            type="checkbox"
+                            name="age_confirmation"
+                            onChange={handleChange}
+                        />
                     </div>
                     <br />
-                    </>
-                ) : (
-                    ""
-                )}
+                    {errors.age_confirmation ? (
+                        <>
+                            <div className="errors">
+                                {errors.age_confirmation.map((error, i) => (
+                                    <div key={i} className="error">
+                                        {error}
+                                    </div>
+                                ))}
+                            </div>
+                            <br />
+                        </>
+                    ) : (
+                        ""
+                    )}
+                </div>
                 <button>Register</button>
                 <br />
             </form>
