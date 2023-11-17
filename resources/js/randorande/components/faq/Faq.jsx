d0fae644f64190ea4bb6336296eb602ec134b912
Lauren Crisafi <lwcrisafi@gmail.com>
2023-11-17 11:19:52 +0100

    list finished and connected scss complete for now

--- resources/js/randorande/components/faq/Faq.jsx
+++ resources/js/randorande/components/faq/Faq.jsx
@@ -8,7 +8,7 @@ const Faq = () => {
             <section>
                 <h2>1. What is RandoRande?</h2>
                 <p>
-                    RandoRande is a unique date ideas app designed to spark
+                    <strong>RandoRande</strong> is a unique date ideas app designed to spark
                     creativity in your relationship.Whether you're looking for
                     new date ideas or want to create lasting memories,
                     RandoRande has you covered.
@@ -17,61 +17,60 @@ const Faq = () => {
 
             <section>
                 <h2>2. How does RandoRande work?</h2>
-                <ol>
+                <ul>
                     <li>
-                        <h3>Choose Your Search Parameters:</h3>
+                        <h3>1. Choose Your Search Parameters:</h3>
                         <p>
                             Begin by selecting your preferences such as budget,
                             season, time of day, and more.
                         </p>
                     </li>
                     <li>
-                        <h3>Get 3 Random Date Ideas:</h3>
+                        <h3>2. Get 3 Random Date Ideas:</h3>
                         <p>
                             Our database will generate 3 surprise date ideas
                             based on your selected criteria.
                         </p>
                     </li>
                     <li>
-                        <h3>Select Your Date:</h3>
+                        <h3>3. Select Your Date:</h3>
                         <p>
                             Review the options and choose the date that appeals
                             to you the most.
                         </p>
                     </li>
                     <li>
-                        <h3>Reveal Your Date:</h3>
+                        <h3>4. Reveal Your Date:</h3>
                         <p>
                             Experience the excitement of our scratch reveal to
                             uncover the details of your chosen date.
                         </p>
                     </li>
                     <li>
-                        <h3>Go on Your Date:</h3>
+                        <h3>5. Go on Your Date:</h3>
                         <p>
                             Make plans and enjoy your surprise date with your
                             significant other.
                         </p>
                     </li>
                     <li>
-                        <h3>Journal Your Experience:</h3>
+                        <h3>6. Journal Your Experience:</h3>
                         <p>
                             After the date, log your thoughts, upload a photo,
                             and create a lasting memory in your personal
                             journal.
                         </p>
                     </li>
-                </ol>
+                </ul>
                 <section>
                     <h2>
                         3. What information is included in the surprise date
                         ideas?
                     </h2>
                     <p>
-                        Each date suggestion comes with a key featuring icons to
+                        Each date suggestion comes with a key featuring an icon to
                         provide additional details. For example, you might find
-                        icons indicating whether it's advisable not to eat
-                        before, dress comfortably, or any other relevant
+                        icons indicating whether the date will involve food, a game, to dress comfortably, or any other relevant
                         information.
                     </p>
                 </section>
@@ -99,7 +98,7 @@ const Faq = () => {
                         Simply sign up, create a profile, and start customizing
                         your date preferences. The app will guide you through
                         the rest, making it easy to discover and enjoy exciting
-                        new experiences with your significant other.
+                        new experiences with your significant other. <Link to="/register">Click here </Link> to sign up.
                     </p>
                 </section>
                 <section>
@@ -111,16 +110,17 @@ const Faq = () => {
                     </p>
                 </section>
                 <section>
+                    {/* where should contact us lead to? */}
                     <h2>
-                        8.How can I provide feedback or suggest new features?
+                        8. How can I provide feedback or suggest new features?
                     </h2>
                     <p>
                         We'd love to hear from you! Visit our
-                        <Link to="">Contact Us</Link> page to share your
+                        <Link to=""> Contact Us</Link> page to share your
                         feedback, suggestions, or report any issues...
                     </p>
                 </section>
-                {/* do we have ramdomiser button? */}
+                    {/* do we have ramdomiser button? */}
                 <section>
                     <h2>9. Can I be surprised with absolutely anything?</h2>
                     <p>
