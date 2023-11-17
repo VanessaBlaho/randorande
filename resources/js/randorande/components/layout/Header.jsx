a0d387e53f422211d29fd234ca4051b39b05e552
Vanessa Blaho <vanessadblaho@gmail.com>
2023-11-17 17:02:56 +0100

    working on logout

--- resources/js/randorande/components/layout/Header.jsx
+++ resources/js/randorande/components/layout/Header.jsx
@@ -1,9 +1,42 @@
-import React, { useState } from "react";
+import React, { useState, useContext } from "react";
 import { Link } from "react-router-dom";
 //import "../../../../scss/app.scss";
+import UserContext from "../../UserContext";
 
-const Header = () => {
+const Header = (props) => {
     const [menuOpen, setMenuOpen] = useState(false);
+
+    const { user, setUser } = useContext(UserContext);
+
+    const handleLogout = async (event) => {
+        event.preventDefault()
+        // console.log(values);
+
+        // clear the errors
+        // setErrors({});
+
+        // fetch with axios
+        try {
+            // make the AJAX request
+            const response = await axios.post('/logout', values);
+            // get the (already JSON-parsed) response data
+            // const response_data = response.data; // use later for console.log()
+            setUser(null);
+            // ADD NAVIGATE TO MY JOURNAL HERE
+            navigate("/my-journal");
+            
+        } catch (error) {
+            // if the response code is not 2xx (success)
+            console.log("Error Response:", error.response.data.errors);
+            if (error.response.status === 422) {
+                // handle validation errors here
+                setErrors(error.response.data.errors);
+            } else if (error.response.status === 500) {
+                console.log("UNKNOWN ERROR", error.response.data);
+            }
+        }
+    };
+
 
     const toggleMenu = () => {
         setMenuOpen(!menuOpen);
@@ -39,12 +72,20 @@ const Header = () => {
             </div>
             <div className="auth-buttons">
                 <ul>
-                    <li className="auth-button">
-                        <Link to="/login">Login</Link>
-                    </li>
-                    <li className="auth-button">
-                        <Link to="/register">Register</Link>
-                    </li>
+                {!user ? (
+                        <>
+                            <li className="auth-button">
+                                <Link to="/login">Login</Link>
+                            </li>
+                            <li className="auth-button">
+                                <Link to="/register">Register</Link>
+                            </li>
+                        </>
+                    ) : (
+                        <li className="auth-button">
+                            <button onClick={handleLogout}>Logout</button>
+                        </li>
+                    )}
                 </ul>
             </div>
         </header>
