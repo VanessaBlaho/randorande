4d0d8b8e0db74f3a8611100a9341bc38a5106f53
Vanessa Blaho <vanessadblaho@gmail.com>
2023-11-18 12:59:39 +0100

    log out working

--- resources/js/randorande/components/layout/Header.jsx
+++ resources/js/randorande/components/layout/Header.jsx
@@ -2,34 +2,29 @@ import React, { useState, useContext } from "react";
 import { Link } from "react-router-dom";
 //import "../../../../scss/app.scss";
 import UserContext from "../../UserContext";
+import axios from "axios";
 
 const Header = (props) => {
     const [menuOpen, setMenuOpen] = useState(false);
 
     const { user, setUser } = useContext(UserContext);
 
-    const handleLogout = async (event) => {
-        event.preventDefault()
-        // console.log(values);
-
-        // clear the errors
-        // setErrors({});
-
-        // fetch with axios
+    const handleLogout = async ev => {
+        ev.preventDefault();
+      
         try {
-            // make the AJAX request
-            const response = await axios.post('/logout', values);
-            // get the (already JSON-parsed) response data
-            // const response_data = response.data; // use later for console.log()
+           
+            const response = await axios.post('/logout');
+       
             setUser(null);
             // ADD NAVIGATE TO MY JOURNAL HERE
-            navigate("/my-journal");
+            // navigate("/my-journal");
             
         } catch (error) {
-            // if the response code is not 2xx (success)
+           
             console.log("Error Response:", error.response.data.errors);
             if (error.response.status === 422) {
-                // handle validation errors here
+                
                 setErrors(error.response.data.errors);
             } else if (error.response.status === 500) {
                 console.log("UNKNOWN ERROR", error.response.data);
@@ -83,7 +78,8 @@ const Header = (props) => {
                         </>
                     ) : (
                         <li className="auth-button">
-                            <button onClick={handleLogout}>Logout</button>
+                            <Link to="/logout" onClick={handleLogout}>Logout</Link>
+
                         </li>
                     )}
                 </ul>
